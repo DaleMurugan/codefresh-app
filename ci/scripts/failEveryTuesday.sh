293e73599fb9669648fb4e168fd3eac255f5b7c9
Dale Murugan <dalemurugan99@gmail.com>
2022-04-11 14:04:15 +0800

    changed script

--- ci/scripts/failEveryTuesday.sh
+++ ci/scripts/failEveryTuesday.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 if [[ $(date +%u) == 2 ]]
 then
-echo "#it's Tueday --fail"
+echo "# it's Tueday --fail"
 exit 1
 fi 
+echo "# it's not Tueday! -- pass "
 exit 0
--- ci/scripts/failOnThe15th.sh
+++ ci/scripts/failOnThe15th.sh
@@ -4,4 +4,5 @@ then
 echo "#it's the 15th! --fail"
 exit 1
 fi 
+echo "# it's not the 15th! --pass"
 exit 0
